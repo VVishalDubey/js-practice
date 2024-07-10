36cdcee913392f13d563a12bb75ed02afca43838
Vishal Dubey <vishal.dubey@aquatraxsolutions.in>
2024-07-10 16:52:58 +0530

    first commit

---
+++ practice.js
@@ -0,0 +1,13 @@
+// for(let i=5; i<=50;i=i+5){
+//     console.log(i);
+// }
+// for(let i=1;i<=3;i++){
+//     for(let j=1;j<=3;j++){
+//         console.log(j);
+//     }
+// }
+let i=0;
+while(i<=20){
+console.log(i);
+i++;
+}
